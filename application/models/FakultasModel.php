1d9e217dd03182df85432ba8d477c70858beb0ff
My_Github <dewadannyswara@gmail.com>
2026-06-10 12:56:37 +0700

    Menambahkan file Fakultas.php dan FakultasModel.php di controllers

--- application/models/FakultasModel.php
+++ application/models/FakultasModel.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class MahasiswaModel extends CI_Model {
+class FakultasModel extends CI_Model {
     public function getAll()
 	{
         return $this->db->get('mahasiswa')->result_array();
