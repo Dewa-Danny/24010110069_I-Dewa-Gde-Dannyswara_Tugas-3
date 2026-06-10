3e21ee1cf92189743e5e916a0fdf7c7c7aa8a799
My_Github <dewadannyswara@gmail.com>
2026-06-10 16:56:39 +0700

    Memodifikasi codingan di file index.php di views

--- application/views/fakultas/index.php
+++ application/views/fakultas/index.php
@@ -1,9 +1,9 @@
 <div class="card shadow border-0 mb-4">
 	<div class="card-header bg-secondary text-white d-flex flex-column flex-md-row justify-content-between align-items-md-center gap-2">
 		<div>
-			<h5 class="mb-0 fw-bold">Data Mahasiswa</h5>
+			<h5 class="mb-0 fw-bold">Data Fakultas</h5>
 		</div>
-		<a class="btn btn-primary btn-lg fw-bold" href="<?php echo base_url('mahasiswa/tambah') ?>">Tambah</a>
+		<a class="btn btn-primary btn-lg fw-bold" href="<?php echo base_url('fakultas/tambah') ?>">Tambah</a>
 	</div>
 	<div class="card-body">
 		<div class="table-responsive">
@@ -11,24 +11,20 @@
 				<thead class="table-dark">
 					<tr>
 						<td>No.</td>
-						<td>NIM</td>
 						<td>Nama</td>
-						<td>Email</td>
 						<td>Aksi</td>
 					</tr>
 				</thead>
 				<tbody>
-                    <?php foreach ($mahasiswa as $key => $value): ?>
+                    <?php foreach ($fakultas as $key => $value): ?>
                         <tr>
                             <td><?php echo $key+1 ?>.</td>
-                            <td><?php echo $value['mahasiswa_nim'] ?></td>
-                            <td><?php echo $value['mahasiswa_nama'] ?></td>
-                            <td><?php echo $value['mahasiswa_email'] ?></td>
+                            <td><?php echo $value['fakultas_name'] ?></td>
                             <td>
-                                <a class="btn btn-warning btn-sm" href="<?php echo base_url('mahasiswa/ubah/'.$value['mahasiswa_id']) ?>">
+                                <a class="btn btn-warning btn-sm" href="<?php echo base_url('fakultas/ubah/'.$value['fakultas_id']) ?>">
 									<i class="bi bi-pencil-square"></i>
 								</a>
-								<a class="btn btn-danger btn-sm btn-hapus" href="<?php echo base_url('mahasiswa/hapus/'.$value['mahasiswa_id']) ?>">
+								<a class="btn btn-danger btn-sm btn-hapus" href="<?php echo base_url('fakultas/hapus/'.$value['fakultas_id']) ?>">
 									<i class="bi bi-trash"></i>
 								</a>
                             </td>
