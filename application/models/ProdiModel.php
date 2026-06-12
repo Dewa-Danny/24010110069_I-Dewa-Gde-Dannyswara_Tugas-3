ffdaecdfa17ca63ee30a1055e08c4cc214c5cf76
My_Github <dewadannyswara@gmail.com>
2026-06-12 14:49:21 +0700

    Memodifikasi kode ProdiModel.php

--- application/models/ProdiModel.php
+++ application/models/ProdiModel.php
@@ -4,28 +4,31 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class ProdiModel extends CI_Model {
     public function getAll()
 	{
-        return $this->db->get('fakultas')->result_array();
+        $this->db->select('prodi.*,fakultas.fakultas_name');
+		$this->db->from('prodi');
+		$this->db->join('fakultas', 'fakultas.fakultas_id = prodi.fakultas_id', 'left');
+		return $this->db->get()->result_array();
 	}
 
 	public function getById($id)
 	{
-		return $this->db->get_where('fakultas', ['fakultas_id' => $id])->row_array();
+		return $this->db->get_where('prodi', ['prodi_id' => $id])->row_array();
 	}
 
 	public function insert($data)
 	{
-		return $this->db->insert('fakultas', $data);
+		return $this->db->insert('prodi', $data);
 	}
 
 	public function update($id, $data)
 	{
-		$this->db->where('fakultas_id', $id);
-		return $this->db->update('fakultas', $data);
+		$this->db->where('prodi_id', $id);
+		return $this->db->update('prodi', $data);
 	}
 
 	public function delete($id)
 	{
-		$this->db->where('fakultas_id', $id);
-		return $this->db->delete('fakultas');
+		$this->db->where('prodi_id', $id);
+		return $this->db->delete('prodi');
 	}
 }
